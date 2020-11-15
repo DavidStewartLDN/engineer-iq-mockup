51a7167865df8237006fee6a0c13b267f674905d
DavidStewartLDN <davidstewart.work@gmail.com>
2020-11-15 21:58:46 +0000

    Completed basic Contact page

--- src/pages/contactpage/contactpage.component.jsx
+++ src/pages/contactpage/contactpage.component.jsx
@@ -5,15 +5,15 @@ import './contactpage.styles.scss'
 const ContactPage = () => (
   <div className='services-container'>
     <div className='services-box'>
-      <h1 className='box'> Consultancy Rates </h1>
+      <h3 className='box'> Consultancy Rates <br/> RFQ Development <br/> Bid Qualification <br/> Troubleshooting</h3>
       <a href={"mailto:sales@engineeriq.co.uk"} className='email'> sales@engineeriq.co.uk </a>
     </div>
     <div className='services-box'>
-      <h1 className='box'> Discuss our process </h1>
+      <h3 className='box'> Discuss our process <br/> Checkout out our skills <br/> Resources & <br/> Experience </h3>
       <a href={"mailto:info@engineeriq.co.uk "} className='email'> info@engineeriq.co.uk </a>
     </div>
     <div className='services-box'>
-      <h1 className='box'> Payment Queries </h1>
+      <h3 className='box'><br/> Payment Queries <br/> Invoice Queries <br/><br/> </h3>
       <a href={"mailto:accounts@engineeriq.co.uk"} className='email'> accounts@engineeriq.co.uk </a>
     </div>
   </div>
