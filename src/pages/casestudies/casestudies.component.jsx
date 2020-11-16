766ad7994a042db8b59391515b5db55e454d3eb5
DavidStewartLDN <davidstewart.work@gmail.com>
2020-11-16 09:53:07 +0000

    Added Case Studies page

---
+++ src/pages/casestudies/casestudies.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './casestudies.styles.scss'
+
+const CaseStudiesPage = () => (
+  <div className='case-studies-container'>
+    <div className='services-box'>
+      <h3 className='box'> 
+        Design to Tender<br/>
+        RFQ Development<br/>
+        Vendor Assessment<br/>
+        Equipment Specification<br/>
+        Equipment Inspection<br/>
+        Factory Acceptance Testing<br/>
+        Site surveys<br/>
+        Support for Hazop</h3>
+    </div>
+  </div>
+)
+
+export default CaseStudiesPage;
