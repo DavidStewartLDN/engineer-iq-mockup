96eac37fcd4279aeec01fae9292bdaf6dff995b9
DavidStewartLDN <davidstewart.work@gmail.com>
2020-11-17 22:35:47 +0000

    Services page with buttons showing correctly

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 import './homepage.styles.scss'
 
-import EngineerIqProcess from '../../assets/EngineerIqProcess.png';
+import Landing from '../../assets/landing.png';
 
 const HomePage = () => (
   <div className='homepage'>
     <h1> BUSINESS CONSULTING SERVICE TO THE FOOD & DRINKS INDUSTRY FOR LARGE & SMALL PROJECTS</h1>
-    <h1>OUR PROCESS TO DRIVE CHANGE & PERFORMANCE IMPROVEMENT</h1>
-    <img className='engineerIqProcess' alt='Engineer iQ Process' src={EngineerIqProcess} />
+    <img className='landing' alt='Engineer iQ services' src={Landing} />
     <h2>We provide independent advice to food and drink businesses to assist them with developing or improving their process & operations. Our focus is on carbon neutral solutions and zero waste operations. Our team offers conceptual design, performance improvement and project support services. Our design expertise covers Automation, Process & Utilities, Manufacturing Operations, Equipment Selection & Project Planning. Backed up by over 80 years working experience in Dairy, Food, Beverage & Brewing.
     </h2>
   </div>
--- src/pages/servicespage/servicespage.component.jsx
+++ src/pages/servicespage/servicespage.component.jsx
@@ -2,11 +2,25 @@ import React from 'react';
 
 import './servicespage.styles.scss'
 
-import ServicesPlaceholder from '../../assets/services_placeholder.png';
+import Automation from '../../assets/services_images/services_automation.png';
 
 const ServicesPage = () => (
   <div className='services-container'>
-    <img className='placeholder' alt='Services offered' src={ServicesPlaceholder} />
+    <div className='buttons-container'>
+      <div className='services-box'>
+        <h3 className='box'> Automation</h3>
+      </div>
+      <div className='services-box'>
+        <h3 className='box'> Operation </h3>
+      </div>
+      <div className='services-box'>
+        <h3 className='box'> Process</h3>
+      </div>
+      <div className='services-box'>
+        <h3 className='box'> Utilities</h3>
+      </div>
+    </div>
+    <img className='placeholder' alt='Services offered' src={Automation} />
   </div>
 )
 
